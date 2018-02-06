7e6ef4180a9dc96dbfdb4b77c13cd4130564ac7d
Victor Alves <vsilva.programador@gmail.com>
2018-02-06 20:33:04 -0200

    Improving tests

--- simple-counter/src/__tests__/Redux.spec.js
+++ simple-counter/src/__tests__/Redux.spec.js
@@ -11,10 +11,12 @@ it('testing increment', () => {
   });
 
   console.log('1st INCREMENT -- COUNT:', store.getState().count);
-  
+  expect(store.getState().count).toEqual(1);
+
   store.dispatch(increment());
 
   console.log('2nd INCREMENT -- COUNT:', store.getState().count);
+  expect(store.getState().count).toEqual(2);
 
 });
 
@@ -28,10 +30,25 @@ it('testing decrement', () => {
   });
 
   console.log('1st DECREMENT -- COUNT:', store.getState().count);
+  expect(store.getState().count).toEqual(1);
 
   store.dispatch(decrement());
 
   console.log('2nd DECREMENT -- COUNT:', store.getState().count);
+  expect(store.getState().count).toEqual(0);
+});
+
+it('testing action types', () => {
+  const incrementAction = {
+    type: 'INCREMENT'
+  };
+
+  const decrementAction = {
+    type: 'DECREMENT'
+  };
+
+  expect(increment()).toEqual(incrementAction);
+  expect(decrement()).toEqual(decrementAction);
 });
 
 it('testing invalid action type', () => {
@@ -44,4 +61,5 @@ it('testing invalid action type', () => {
   });
 
   console.log('AFTER INVALID ACTION TYPE -- COUNT:', store.getState().count);
+  expect(store.getState().count).toEqual(0);
 });
